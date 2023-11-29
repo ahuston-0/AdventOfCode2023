e258484a3203c853b66606d41f34880dfd45d193
ahuston-0 <aliceghuston@gmail.com>
2023-11-28 22:11:51 -0500

    add input file, cleanup day generation
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -16,8 +16,13 @@ pub fn init_logs() {
 
 pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<BufReader<File>>>
 where
-    P: AsRef<Path>,
+    P: AsRef<Path> + std::fmt::Debug + Copy ,
 {
     let file = File::open(filename)?;
+    if file.metadata().unwrap().len() < 50 {
+        log::error!("{:?} is less than 50 bytes, chances are you forgot to supply the input. Update the file and try again.", filename);
+        quit::with_code(1);
+    }
+
     Ok(BufReader::new(file).lines())
 }
--- template-day/src/main.rs
+++ template-day/src/main.rs
@@ -1,3 +1,31 @@
+use std::path::PathBuf;
+
+use common::{init_logs, read_lines};
+
 fn main() {
-    println!("Hello, world!");
+    init_logs();
+    puzzle1();
+    puzzle2();
+}
+
+fn puzzle1() {
+    let mut input_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    input_path.push("resources/input");
+
+    for line in read_lines(input_path.as_path()).unwrap() {
+        // Make below variable "entry" instead once starting the puzzle
+        // This is mostly to avoid clippy complaining x50
+        let _entry = line.unwrap();
+    }
+}
+
+fn puzzle2() {
+    let mut input_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    input_path.push("resources/input");
+
+    for line in read_lines(input_path.as_path()).unwrap() {
+        // Make below variable "entry" instead once starting the puzzle
+        // This is mostly to avoid clippy complaining x50
+        let _entry = line.unwrap();
+    }
 }
