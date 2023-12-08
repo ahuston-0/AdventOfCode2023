ed091b0294a008c5d2998229f2ad86cc1d1a323f
ahuston-0 <aliceghuston@gmail.com>
2023-12-08 01:34:34 -0500

    Revert from brute force for day 8 p2
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,4 +1,5 @@
 use common::{init_logs, read_lines};
+use num::Integer;
 use regex::Regex;
 use std::{
     collections::HashMap,
@@ -19,7 +20,7 @@ fn puzzle1(input_path: &Path) -> u32 {
 
     let mut num_steps = 0;
 
-    let input: Vec<_> = read_lines(&input_path)
+    let input: Vec<_> = read_lines(input_path)
         .unwrap()
         .map(|d| d.unwrap())
         .collect();
@@ -31,7 +32,7 @@ fn puzzle1(input_path: &Path) -> u32 {
     let mut node_map: HashMap<&str, (&str, &str)> = HashMap::new();
 
     input.iter().skip(2).for_each(|line| {
-        let caps = node_re.captures(&line).unwrap();
+        let caps = node_re.captures(line).unwrap();
         let node = caps.name("node").unwrap().as_str();
         let left = caps.name("left").unwrap().as_str();
         let right = caps.name("right").unwrap().as_str();
@@ -55,10 +56,8 @@ fn puzzle1(input_path: &Path) -> u32 {
     num_steps
 }
 
-fn puzzle2(input_path: &Path) -> u32 {
-    let mut num_steps = 0;
-
-    let input: Vec<_> = read_lines(&input_path)
+fn puzzle2(input_path: &Path) -> u64 {
+    let input: Vec<_> = read_lines(input_path)
         .unwrap()
         .map(|d| d.unwrap())
         .collect();
@@ -70,14 +69,13 @@ fn puzzle2(input_path: &Path) -> u32 {
     let mut node_map: HashMap<&str, (&str, &str)> = HashMap::new();
 
     input.iter().skip(2).for_each(|line| {
-        let caps = node_re.captures(&line).unwrap();
+        let caps = node_re.captures(line).unwrap();
         let node = caps.name("node").unwrap().as_str();
         let left = caps.name("left").unwrap().as_str();
         let right = caps.name("right").unwrap().as_str();
         node_map.insert(node, (left, right));
     });
-
-    let mut currents: Vec<_> = node_map
+    let currents: Vec<_> = node_map
         .keys()
         .cloned()
         .filter(|node| node.ends_with('A'))
@@ -85,25 +83,27 @@ fn puzzle2(input_path: &Path) -> u32 {
 
     let mut current_instruction = 0;
 
-    while !is_on_pos(&currents, 'Z') {
-        log::debug!("currents {:?}", currents);
-        for i in 0..currents.len() {
-            let node = node_map.get(currents[i]).unwrap();
-            currents[i] = match directions.chars().nth(current_instruction).unwrap() {
+    let mut cycles: Vec<u64> = Vec::new();
+
+    currents.iter().cloned().for_each(|c| {
+        let mut current = c;
+        let mut num_steps = 0;
+        while !current.ends_with('Z') {
+            let node = node_map.get(current).unwrap();
+            current = match directions.chars().nth(current_instruction).unwrap() {
                 'L' => node.0,
                 'R' => node.1,
+
                 _ => panic!("invalid input"),
-            }
+            };
+            num_steps += 1;
+            current_instruction = (current_instruction + 1) % directions.len();
         }
-        current_instruction = (current_instruction + 1) % directions.len();
-        num_steps += 1;
-    }
+        cycles.push(num_steps);
+    });
+    let num_steps = cycles.iter().cloned().reduce(|a, b| a.lcm(&b)).unwrap();
     log::info!("num steps {num_steps}");
     num_steps
-}
-
-fn is_on_pos(nodes: &[&str], node_end: char) -> bool {
-    nodes.iter().all(|node| node.ends_with(node_end))
 }
 
 #[cfg(test)]
